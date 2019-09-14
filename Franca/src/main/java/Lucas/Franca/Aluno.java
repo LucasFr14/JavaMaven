19e22b4253502a5a358639fd32f384d980a22dc6
lucas <lucas@DESKTOP-FP01CHS.localdomain>
2019-09-14 17:45:21 -0300

    Aep do Arthur.

---
+++ Franca/src/main/java/Lucas/Franca/Aluno.java
@@ -0,0 +1,23 @@
+package Lucas.Franca;
+
+public class Aluno {
+	private String nome;
+	private Double numero;
+	
+	public Aluno(String nome, Double numero) {
+		this.nome = nome;
+		this.numero = numero;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public Double getNumero() {
+		return numero;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setNumero(Double numero) {
+		this.numero = numero;
+	}
+}
---
+++ Franca/src/main/java/Lucas/Franca/Avaliação.java
@@ -0,0 +1,12 @@
+package Lucas.Franca;
+
+public class Avaliação {
+	private String avaliacao;
+	
+	public Avaliação(String avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+	public String getAvaliacao() {
+		return avaliacao;
+	}
+}
